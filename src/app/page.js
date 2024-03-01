1666178c995bef136848268284021262e8a0bff1
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-01 17:57:14 -0300

    remove example files

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Changas</h1>
-    </main>
-  );
-}
-
-export default Home;
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+function Home () {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>Changas</h1>
+    </main>
+  )
+}
+
+export default Home
