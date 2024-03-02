6ab94a6a8a195c2256b64032dd965d19b190ef42
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-02 14:08:09 -0300

    style tags

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,14 @@ import Header from '@/components/header'
 import Footer from '@/components/footer'
 import Link from 'next/link'
 import Divider from '@/ui/divider'
-import Tag from '@/ui/tag'
+
+function Tag ({ nombre }) {
+  return (
+    <span className='font-semibold px-6 py-2 border-2 border-brand6 rounded-full text-brand6 hover:text-brand8 hover:bg-brand6'>
+      {nombre}
+    </span>
+  )
+}
 
 function Home () {
   return (
--- src/ui/tag.jsx
+++
@@ -1,9 +0,0 @@
-function Tag ({ nombre }) {
-  return (
-    <span className='font-semibold px-6 py-2 border-2 border-brand6 rounded-full text-brand6 hover:text-brand8 hover:bg-brand6'>
-      {nombre}
-    </span>
-  )
-}
-
-export default Tag
