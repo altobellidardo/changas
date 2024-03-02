0ee745d393d8a55413c5a037bf5fe108e3148681
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-01 23:34:06 -0300

    add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        brand1: '#073B3A',
+        brand2: '#095545',
+        brand3: '#0B6E4F',
+        brand4: '#0A7F4C',
+        brand5: '#098F48',
+        brand6: '#08A045',
+        brand7: '#6BBF59',
+        brand8: '#F5F5F5'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
