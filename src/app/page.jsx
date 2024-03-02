1770e0f4e54c37394ed5c68929652ef89d60ce31
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-02 00:40:44 -0300

    improve home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,43 @@
 import Header from '@/components/header'
 import Link from 'next/link'
 
+function Divider () {
+  return (
+    <div className='w-full max-w-[1000px] mx-auto h-[2px] bg-brand1' />
+  )
+}
+
+function Tag ({ nombre }) {
+  return (
+    <span className='font-semibold border-2 border-brand6 text-brand6 hover:bg-brand6 hover:text-brand1 px-6 py-2 rounded-full'>{nombre}</span>
+  )
+}
+
 function Home () {
   return (
     <main className='min-h-screen bg-brand8'>
       <Header />
 
-      <section className='w-full max-w-[1000px] mx-auto my-28'>
-        <h2 className='text-5xl font-semibold'>Contrata a los Mejores. Haz el Trabajo.</h2>
+      <section className='w-full max-w-[1000px] mx-auto my-32'>
+        <h2 className='text-5xl font-semibold mb-2'>Contrata a los Mejores. Haz el Trabajo.</h2>
         <p className='text-2xl'>Conectando profesionales cualificados con clientes. Encuentra a la persona adecuada para tu proyecto o sé contratado por tu experiencia.</p>
-        <div className='flex gap-4 my-5 justify-center items-center'>
+        <div className='flex gap-4 my-5 justify-center items-center mt-12'>
           <Link href='/trabajos' className='font-semibold border-2 border-brand6 text-brand6 rounded-xl px-4 py-2 hover:text-brand1 hover:bg-brand6'>Explorar Trabajos</Link>
           <Link href='/login' className='font-semibold rounded-xl px-4 py-2 border-2 border-brand6 bg-brand6 text-brand8 hover:text-brand1'>Registrarse</Link>
         </div>
       </section>
 
-      <section className='w-full max-w-[1000px] mx-auto my-28 flex gap-4'>
+      <Divider />
+
+      <section className='w-full max-w-[1000px] mx-auto my-32 flex gap-4'>
         <div className='w-1/2'>
-          <h2 className='text-5xl font-semibold'>Tu Plataforma de Confianza</h2>
-          <p className='text-2xl'>Los clientes confían en nuestra plataforma para encontrar profesionales cualificados para sus proyectos. Los profesionales confían en nosotros para conectarse con clientes potenciales y hacer crecer su negocio.</p>
+          <h2 className='text-5xl font-semibold mb-2'>Tu Plataforma de Confianza</h2>
+          <p className='text-2xl text-balance'>Los clientes confían en nuestra plataforma para encontrar profesionales cualificados para sus proyectos. Los profesionales confían en nosotros para conectarse con clientes potenciales y hacer crecer su negocio.</p>
         </div>
 
         <div className='w-1/2 flex gap-7 flex-col'>
           <div>
-            <h3 className='text-xl font-semibold'>Para Clientes</h3>
+            <h3 className='text-xl font-semibold mb-2'>Para Clientes</h3>
             <ul>
               <li> - Encuentra profesionales cualificados para tus proyectos de mejora del hogar</li>
               <li> - Conéctate con electricistas, fontaneros, pintores y más</li>
@@ -32,13 +46,30 @@ function Home () {
           </div>
 
           <div>
-            <h3 className='text-xl font-semibold'>Para Profecionales</h3>
+            <h3 className='text-xl font-semibold mb-2'>Para Profecionales</h3>
             <ul>
               <li> - Sé contratado por tu experiencia</li>
               <li> - Muestra tus habilidades y experiencia</li>
               <li> - Haz crecer tu base de clientes</li>
             </ul>
           </div>
+        </div>
+      </section>
+
+      <Divider />
+
+      <section className='w-full max-w-[1000px] mx-auto my-32'>
+        <h1 className='text-5xl font-semibold mb-2'>Únete a la Plataforma</h1>
+        <p className='text-2xl text-balance'>Los clientes confían en nuestra plataforma para encontrar profesionales cualificados para sus proyectos. Los profesionales confían en nosotros para conectarse con clientes potenciales y hacer crecer su negocio.</p>
+
+        <div className='flex w-3/4 mx-auto gap-4 my-5 justify-center items-center mt-12 flex-wrap'>
+          <Tag nombre='Electricista' />
+          <Tag nombre='Plomero' />
+          <Tag nombre='Pintor' />
+          <Tag nombre='Servicio doméstico' />
+          <Tag nombre='Carpintero' />
+          <Tag nombre='Cerrajero' />
+          <Tag nombre='Cocinero' />
         </div>
       </section>
 
