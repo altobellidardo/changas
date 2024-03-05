a453fe71a54f49db87408a44a0fffd4e7a600060
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-03-05 09:25:16 -0300

    reorder tailwind classes

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 function ContactPage () {
   return (
-    <main className='min-h-screen bg-brand1 text-brand8 p-10'>
-      <Link href='/' className='font-semibold hover:underline underline-offset-4'>Ir al inicio</Link>
-      <h1 className='text-4xl font-semibold mt-10'>¿En que podemos ayudarte?</h1>
+    <main className='min-h-screen bg-brand1 p-10 text-brand8'>
+      <Link href='/' className='font-semibold underline-offset-4 hover:underline'>Ir al inicio</Link>
+      <h1 className='mt-10 text-4xl font-semibold'>¿En que podemos ayudarte?</h1>
     </main>
   )
 }
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 function NotFound () {
   return (
-    <section className='min-h-screen flex flex-col justify-center items-center bg-brand1'>
+    <section className='flex min-h-screen flex-col items-center justify-center bg-brand1'>
       <h1 className='text-6xl text-brand8'>404 Error</h1>
       <h2 className='text-2xl text-brand8'>Página no encontrada</h2>
       <Image src='/logo.svg' width={100} height={100} alt='logo' className='my-10' />
-      <Link href='/' className='font-semibold border-2 border-brand6 text-brand6 rounded-xl px-4 py-2 hover:text-brand1 hover:bg-brand6'>
+      <Link href='/' className='rounded-xl border-2 border-brand6 px-4 py-2 font-semibold text-brand6 hover:bg-brand6 hover:text-brand1'>
         Ir al inicio
       </Link>
     </section>
