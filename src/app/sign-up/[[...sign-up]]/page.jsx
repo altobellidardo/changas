427a69d4df4852132fb730669d6452b398e88200
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-06 18:38:34 -0300

    add auth styles

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,8 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
+import '@/app/clerk-styles.css'
 
 export default function Page () {
   return (
-    <div className='flex justify-center py-24'>
+    <div className='flex justify-center py-24  bg-brand1 min-h-screen'>
       <SignUp />
     </div>
   )
