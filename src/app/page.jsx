b04eab2d84cb892a9728ff787d0b8eb497247822
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-13 07:56:19 -0300

    test camelcase components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header'
+import Header from '@/components/header'
 import Footer from '@/components/footer'
 import Link from 'next/link'
 import Divider from '@/components/ui/divider'
