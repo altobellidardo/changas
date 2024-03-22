026c6a16901f85debbab003ca506b995f2d020a5
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-22 18:39:45 -0300

    findworker changas

--- src/app/findworker/page.jsx
+++ src/app/findworker/page.jsx
@@ -8,9 +8,9 @@ async function FindWorkerPage () {
     <>
       {
         categories.map((item) => (
-          <div key={item.id_category} className='p-4 border-2 m-2 w-96'>
-            <Link href={{ pathname: `/findworker/${item.id_category}` }}>Encontrar: {item.id_category}s</Link>
-            <div>Nombre del oficio: {item.id_category} Descripcion: {item.descripcion}</div>
+          <div key={item.name} className='p-4 border-2 m-2 w-96'>
+            <Link href={{ pathname: `/findworker/${item.name}` }}>Encontrar: {item.name}s</Link>
+            <div>Descripcion: {item.description}</div>
           </div>
         ))
       }
