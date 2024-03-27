f1273eaadfc6135bdd95ef484bbc643d0672825a
Maximo Rulli <rullimaximoeduardo@gmail.com>
2024-03-27 10:03:26 -0300

    erased console logs for ease of visualization

--- src/actions/getJobs.js
+++ src/actions/getJobs.js
@@ -5,7 +5,5 @@ import supabase from '@/libs/supabase/server'
 export async function getJobs (IdUser) {
   const { data: user } = await supabase.from('workers').select().eq('id_user', IdUser)
 
-  console.log(user)
-
   return user
 }
--- src/actions/getOffers.js
+++ src/actions/getOffers.js
@@ -5,7 +5,5 @@ import supabase from '@/libs/supabase/server'
 export async function getOffers (IdUser) {
   const { data: user } = await supabase.from('proposals').select().eq('id_user', IdUser)
 
-  console.log(user)
-
   return user
 }
