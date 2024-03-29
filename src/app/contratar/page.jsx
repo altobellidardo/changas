ea1e4b04ae05d4719e5df32a034938a2eb750e06
Dardo Altobelli <altobellidardo@gmail.com>
2024-03-29 12:47:46 -0300

    style contratar page

--- src/app/contratar/page.jsx
+++ src/app/contratar/page.jsx
@@ -1,22 +1,43 @@
 import { getCategories } from '@/actions/getCategories'
+import Footer from '@/components/footer'
+import Header from '@/components/header/header'
 import Link from 'next/link'
 
 export const fetchCache = 'force-no-store'
+
+function Category ({ category }) {
+  return (
+    <div className='p-4 border-2 rounded-xl border-brand6 bg-brand6/10'>
+      <Link className='text-xl font-bold hover:underline' href={`/contratar/${category.name}`}>
+        {category.name}s
+      </Link>
+      <p>{category.description}</p>
+    </div>
+  )
+}
 
 async function FindWorkerPage () {
   const categories = await getCategories()
 
   return (
-    <>
-      {
-        categories.map((item) => (
-          <div key={item.name} className='p-4 border-2 m-2 w-96'>
-            <Link href={{ pathname: `/contratar/${item.name}` }}>Encontrar: {item.name}s</Link>
-            <div>Descripcion: {item.description}</div>
-          </div>
-        ))
-      }
-    </>
+    <main className='min-h-screen flex flex-col justify-between'>
+      <Header />
+      <section className='pt-10 max-w-[80vw] mx-auto my-10'>
+        <h1 className='text-3xl font-bold'>Contratar </h1>
+        <p className='mb-4'>
+          Hay {categories.length} {categories.length === 1 ? 'oficio' : 'oficios'} disponibles
+        </p>
+
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+          {
+            categories.map((item) => (
+              <Category key={item.name} category={item} />
+            ))
+          }
+        </div>
+      </section>
+      <Footer />
+    </main>
   )
 }
 
