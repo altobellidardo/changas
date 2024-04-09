5bbee995ea0d434440fbcc208a90f5c113c35d84
Maximo Rulli <rullimaximoeduardo@gmail.com>
2024-04-09 12:41:46 -0300

    updated rating system to use Bayesian statistics

--- src/actions/getRatings.js
+++ src/actions/getRatings.js
@@ -9,16 +9,18 @@ export async function getRatings (IdUser, jobs) {
     .eq('id_reviewed_user', IdUser).in('category', jobsNames)
 
   // Create an object to hold the total score and count for each category
+  const R = 2.4
+  const W = 5
   const categoryData = {}
   ratings.forEach(rating => {
     if (!categoryData[rating.category]) {
-      categoryData[rating.category] = { total: 0, count: 0 }
+      categoryData[rating.category] = { total: (R * W), count: W }
     }
     categoryData[rating.category].total += rating.score
     categoryData[rating.category].count++
   })
 
-  // Calculate the average score for each category
+  // Calculate the average score for each category based on Bayesian probability (https://stackoverflow.com/questions/2495509/how-to-balance-number-of-ratings-versus-the-ratings-themselves)
   const averageRatings = {}
   for (const category in categoryData) {
     averageRatings[category] = (categoryData[category].total / categoryData[category].count).toFixed(1)
