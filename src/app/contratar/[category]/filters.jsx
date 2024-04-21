f5919309cb1629951480628100158004c33cf91b
Dardo Altobelli <altobellidardo@gmail.com>
2024-04-21 12:51:01 -0300

    add filter menu

--- src/app/contratar/[category]/filters.jsx
+++ src/app/contratar/[category]/filters.jsx
@@ -8,6 +8,7 @@ function Filters ({ category }) {
   const [workers, setWorkers] = useState([])
   const [filter, setFilter] = useState({})
   const [loading, setLoading] = useState(true)
+  const [open, setOpen] = useState(false)
 
   const changeFilter = (event) => {
     event.preventDefault()
@@ -50,52 +51,99 @@ function Filters ({ category }) {
 
   return (
     <>
-      <form onSubmit={changeFilter}>
-        <div>
-          <span>Nombre y apellido</span>
-          <input
-            type='text'
-            name='name'
-            placeholder='Juan Perez'
-          />
-        </div>
-        <div>
-          <span>precio por hora</span>
-          <input
-            type='number'
-            min='0'
-            name='hourly_price'
-            placeholder='500'
-          />
-        </div>
-        <div>
-          <span>N° de empleados</span>
-          <input
-            type='number'
-            min='0'
-            name='employees'
-            placeholder='5'
-          />
-        </div>
-        <div>
-          <span>Puntaje</span>
-          <input
-            type='number'
-            min='0'
-            max='5'
-            name='score'
-            placeholder='5'
-          />
-        </div>
-        <div>
-          <button type='reset' className='bg-brand6/10 p-2 hover:bg-brand6/40 rounded-md'>
-            Limpiar filtros
+      {
+        open
+          ? (
+            <button>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+                onClick={() => setOpen(false)}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+            )
+          : (
+            <button
+              onClick={() => setOpen(true)}
+              className='flex items-center gap-2'
+            >
+              <p>Filtrar</p>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+                />
+              </svg>
+            </button>
+            )
+        }
+
+      {open && (
+        <form onSubmit={changeFilter} className='flex max-w-3xl flex-col gap-4 mb-4'>
+          <div>
+            <span>Nombre y apellido</span>
+            <input
+              type='text'
+              name='name'
+              placeholder='Juan Perez'
+            />
+          </div>
+          <div>
+            <span>precio por hora</span>
+            <input
+              type='number'
+              min='0'
+              name='hourly_price'
+              placeholder='500'
+            />
+          </div>
+          <div>
+            <span>N° de empleados</span>
+            <input
+              type='number'
+              min='0'
+              name='employees'
+              placeholder='5'
+            />
+          </div>
+          <div>
+            <span>Puntaje</span>
+            <input
+              type='number'
+              min='0'
+              max='5'
+              name='score'
+              placeholder='5'
+            />
+          </div>
+          <div>
+            <button type='reset' className='bg-brand6/10 p-2 hover:bg-brand6/40 rounded-md'>
+              Limpiar filtros
+            </button>
+          </div>
+          <button type='submit' className='bg-brand6/10 p-2 hover:bg-brand6/40 rounded-md'>
+            Filtrar
           </button>
-        </div>
-        <button type='submit' className='bg-brand6/10 p-2 hover:bg-brand6/40 rounded-md'>
-          Filtrar
-        </button>
-      </form>
+        </form>
+      )}
 
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
         {
