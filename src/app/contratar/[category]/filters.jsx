ccd242adfccc925a59032e23284afc6e63cab3ef
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-04-27 10:32:59 -0300

    fixed error of bad import

--- src/app/contratar/[category]/filters.jsx
+++ src/app/contratar/[category]/filters.jsx
@@ -3,18 +3,8 @@
 
 import { useEffect, useState } from 'react'
 import Worker from './Worker'
-import { cookies } from 'next/headers'
-import checkUser from '@/utils/checkUser'
-import { redirect } from 'next/navigation'
 
-function Filters ({ category }) {
-  const token = cookies().get('token')
-  const isAuthenticated = checkUser(token?.value)
-  if (!isAuthenticated) redirect('/')
-
-  // Retrieve data from JWT
-  const { id_user: IdUser } = isAuthenticated
-
+function Filters ({ category, IdUser }) {
   const [workers, setWorkers] = useState([])
   const [filter, setFilter] = useState({})
   const [loading, setLoading] = useState(true)
