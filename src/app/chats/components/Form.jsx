5ee209a40c08cc550526d9664003044e31dff172
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-05-23 15:32:28 -0300

    add form in chat

--- src/app/chats/components/Form.jsx
+++ src/app/chats/components/Form.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 import UpArrowIcon from '@/components/icons/UpArrow'
 
-export default function Form ({ IdChat, IdUser, OtherUser, Username, Username2, history, UserNumber }) {
+function ChatForm ({ IdChat, IdUser, OtherUser, Username, Username2, history, UserNumber }) {
   const router = useRouter()
   const [enviando, setEnviando] = useState(false)
   const [open, setOpen] = useState(false)
@@ -41,9 +41,9 @@ export default function Form ({ IdChat, IdUser, OtherUser, Username, Username2, 
   }
 
   return (
-    <>
-      <section className={open ? 'block' : 'hidden'}>
-        <div className='flex gap-2'>
+    <section className='p-2 bg-brand5 flex flex-col gap-1 fixed bottom-0 left-0 w-screen justify-center'>
+      <div className={`transition-all ${open ? 'block transition-transform' : 'hidden'}`}>
+        <div className='text-brand8 max-w-[1200px] mx-auto flex gap-2 justify-center'>
           <Link
             href={{ pathname: `/chats/${IdChat}/nuevocontrato`, query: { IdUser, OtherUser, IdChat } }}
             className='bg-brand3 hover:outline text-white py-2 px-4 rounded-full'
@@ -57,28 +57,37 @@ export default function Form ({ IdChat, IdUser, OtherUser, Username, Username2, 
             Contratos
           </Link>
         </div>
-      </section>
-      <form
-        onSubmit={handlemessage}
-        className='p-2 bg-brand5 flex gap-1 fixed bottom-0 left-0 w-screen justify-center'
-      >
-        <button type='button' className='bg-brand3 hover:outline text-brand8 py-1 px-2 rounded-xl' onClick={() => setOpen(!open)}>
-          <UpArrowIcon />
-        </button>
-        <input
-          type='text'
-          name='message'
-          placeholder='escribe aquí...'
-          className='p-2 outline-brand8 rounded-xl bg-brand3 hover:outline focus:outline-2 text-brand8 w-[90%] max-w-[1200px]'
-        />
-        <button
-          type='submit'
-          disabled={enviando}
-          className='bg-brand3 hover:outline text-brand8 text-sm py-1 px-2 rounded-xl disabled:opacity-50'
+      </div>
+      <div className=''>
+        <form
+          onSubmit={handlemessage}
+          className='p-2 bg-brand5 flex gap-1 max-w-[1200px] justify-center mx-auto'
         >
-          Enviar
-        </button>
-      </form>
-    </>
+          <button
+            type='button'
+            className='bg-brand3 hover:outline text-brand8 py-1 px-2 rounded-xl'
+            onClick={() => setOpen(!open)}
+            style={{ transform: open ? 'rotate(180deg)' : 'rotate(0deg)' }}
+          >
+            <UpArrowIcon />
+          </button>
+          <input
+            type='text'
+            name='message'
+            placeholder='escribe aquí...'
+            className='p-2 outline-brand8 rounded-xl bg-brand3 hover:outline focus:outline-2 text-brand8 w-[90%] max-w-[1200px]'
+          />
+          <button
+            type='submit'
+            disabled={enviando}
+            className='bg-brand3 hover:outline text-brand8 text-sm py-1 px-2 rounded-xl disabled:opacity-50'
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
+
+export default ChatForm
