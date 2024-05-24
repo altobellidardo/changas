dfb1d5b87c238c03ad897b20defdcb0b16d11f4a
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-05-23 21:09:54 -0300

    deleted unused function

--- src/actions/getWorkers.js
+++
@@ -1,7 +0,0 @@
-import supabase from '@/libs/supabase/server'
-
-export async function getWorkers (Category) {
-  const { data } = await supabase.from('workers').select().eq('category', Category).order('score', { ascending: false })
-
-  return data
-}
