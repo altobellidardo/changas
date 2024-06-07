e902e55de7dd17f588ac91e1ed8ce322ee1851f9
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-06-07 10:25:15 -0300

    new worker form

--- src/app/contratar/[category]/Worker.jsx
+++ src/app/contratar/[category]/Worker.jsx
@@ -1,37 +1,48 @@
+'use client'
+
+// import PlusIcon from '@/components/icons/PlusIcon'
+import StarIcon from '@/components/icons/Star'
 import Link from 'next/link'
+import { useState } from 'react'
 
 function Worker ({ worker, IdUser }) {
+  const [show, setShow] = useState(false)
+
+  const city = worker.location.split(',')[0]
+  const handleShow = () => setShow(!show)
+
   return (
-    <section>
-      <header>
-        <span className='font-bold '>{worker.username}</span>
-        <div>
-          <span>{worker.location}</span>
-        </div>
-      </header>
-      <footer>
-        <span>${worker.hourly_price}/h</span>
-      </footer>
+    <section className='p-4 py-8 border-b-0 rounded-xl border-brand6 bg-brand6/10 mb-2'>
+      <div className='grid grid-cols-3'>
+        <header>
+          <span className='font-bold'>{worker.username}</span>
+          <div className='flex gap-2'>
+            <span>{city}</span>
+            <span>|</span>
+            <span className='flex gap-1 items-center'>
+              {worker.score}
+              <StarIcon className='size-4 text-brand6' />
+            </span>
+          </div>
+          <span className='text-xl font-bold'>${worker.hourly_price}/h</span>
+        </header>
+        <main className='my-auto'>
+          <p>Hay <span className='font-bold'>{worker.employees}</span> empleado/s</p>
+          <p>Horas de búsqueda:</p>
+          <p className='font-bold'>{worker.attention_hours}</p>
+        </main>
+        <footer className='flex gap-2'>
+          <button className='' onClick={handleShow}>Ver más</button>
+          {worker.id_user !== IdUser &&
+            <Link href={`/chats/nuevochat?idUser2=${worker.id_user}&Username2=${worker.username}`} className='hover:underline'>Contactar</Link>}
+        </footer>
+      </div>
+      {show &&
+        <div className='mt-4'>
+          <span className='opacity-60 text-sm'>Descripción</span>
+          <div>{worker.description}</div>
+        </div>}
     </section>
-    // <div className='p-4 border-2 rounded-xl border-brand6 bg-brand6/10'>
-    //   <span className='opacity-60 text-sm'>Nombre y apellido</span>
-    //   <div>{worker.username}</div>
-    //   <span className='opacity-60 text-sm'>Precio por hora</span>
-    //   <div>${worker.hourly_price}/h</div>
-    //   <span className='opacity-60 text-sm'>N° de empleados</span>
-    //   <div>{worker.employees}</div>
-    //   <span className='opacity-60 text-sm'>Horas de búsqueda</span>
-    //   <div>{worker.attention_hours}</div>
-    //   <span className='opacity-60 text-sm'>Descripción</span>
-    //   <div>{worker.description}</div>
-    //   <span className='opacity-60 text-sm'>Puntaje</span>
-    //   <div>{worker.score}/5</div>
-    //   {/* {worker.id_user !== IdUser
-    //     ? <Link href={`/chats/nuevochat?idUser2=${worker.id_user}&Username2=${worker.username}`} className='hover:underline'>Contactar</Link>
-    //     : undefined} */}
-    //   {worker.id_user !== IdUser &&
-    //     <Link href={`/chats/nuevochat?idUser2=${worker.id_user}&Username2=${worker.username}`} className='hover:underline'>Contactar</Link>}
-    // </div>
   )
 }
 
