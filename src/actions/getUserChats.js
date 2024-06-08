e86e2ae9c653d9180225e3dc3765c27f4b516fb5
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-06-07 22:10:16 -0300

    finished get chats action

--- src/actions/getUserChats.js
+++ src/actions/getUserChats.js
@@ -9,48 +9,35 @@ export async function getUserChats (IdUser) {
     .or(`id_user1.eq.${IdUser}, id_user2.eq.${IdUser}`).order('last_message', { ascending: false })
 
   // Get the user Ids for the contracts filter
-  let OtherUsersIds = chats
-    .filter(chat => chat.id_user1 !== IdUser)
-    .map(chat => chat.id_user1)
-
-  OtherUsersIds = OtherUsersIds.concat(chats
-    .filter(chat => chat.id_user2 !== IdUser)
-    .map(chat => chat.id_user2))
+  const otherUsersIds = chats.reduce((acc, chat) => {
+    if (chat.id_user1 !== IdUser) {
+      acc.push(chat.id_user1)
+    } else {
+      acc.push(chat.id_user2)
+    }
+    return acc
+  }, [])
 
   const contractsColumns = 'id_worker, id_contractor, closed, date'
   const { data: contracts } = await supabase.from('contracts').select(contractsColumns)
-    .or(`and(id_worker.in.(${OtherUsersIds}), id_contractor.eq.${IdUser}), and(id_contractor.in.(${OtherUsersIds}), id_worker.eq.${IdUser})`)
+    .or(`and(id_worker.in.(${otherUsersIds}), id_contractor.eq.${IdUser}), and(id_contractor.in.(${otherUsersIds}), id_worker.eq.${IdUser})`)
     .eq('closed', false)
 
   // Main loop where every chat is mapped to a contract state
   data = chats
-  let lastContractIndex = 0
-  let finishedContracts = false
-
-  console.log(contracts)
-
-  for (let i = 0; i < OtherUsersIds.length; i++) {
-    for (let contractIndex = lastContractIndex; contractIndex < contracts.length; contractIndex++) {
-      console.log(`El índice del contrato es: ${contractIndex}`)
-      const condition1 = OtherUsersIds[i] === contracts[contractIndex].id_contractor
-      const condition2 = OtherUsersIds[i] === contracts[contractIndex].id_worker
+  for (let contractIndex = 0; contractIndex < contracts.length; contractIndex++) {
+    for (let chatIndex = 0; chatIndex < otherUsersIds.length; chatIndex++) {
+      const condition1 = otherUsersIds[chatIndex] === contracts[contractIndex].id_contractor
+      const condition2 = otherUsersIds[chatIndex] === contracts[contractIndex].id_worker
 
       const IdInContract = condition1 || condition2
-      console.log(`Los ids del contrato son: ${contracts[contractIndex].id_contractor} y ${contracts[contractIndex].id_worker}`)
-      console.log(`El id del user es: ${OtherUsersIds[i]}`)
-      console.log(IdInContract)
-      if (contracts.length !== 0 && IdInContract && !finishedContracts) {
-        data[i].open_contract = true
-        data[i].contract_date = contracts[contractIndex].date
-        contractIndex = contracts.length
-        // console.log((OtherUsersIds[i] === contracts[contractIndex].id_contractor) || (OtherUsersIds[i] === contracts[contractIndex].id_worker))
-        // If I did not iterate over the second to last contract I add 1 to the contract index to do not
-        // iterate again over contracts that were already analyzed
-        if (lastContractIndex < contracts.length - 1) {
-          lastContractIndex += 1
-        } else {
-          finishedContracts = true
-        }
+      /* console.log(`Los ids a comparar son: ${contracts[contractIndex].id_contractor} y ${contracts[contractIndex].id_worker}`)
+      console.log(`El id del usuario es: ${otherUsersIds[chatIndex]}`)
+      console.log(IdInContract, chatIndex) */
+      if (contracts.length !== 0 && IdInContract) {
+        data[chatIndex].open_contract = true
+        data[chatIndex].contract_date = contracts[contractIndex].date
+        chatIndex = otherUsersIds.length
       }
     }
   }
