6a25573a180fa744e200837b5e88d3f86946b12e
Maximo Rulli <rullimaximoeduardo@gmail.com>
2024-06-10 14:20:20 -0300

    frontend check of edit forms

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import StarIcon from '@/components/icons/Star'
 import SwitchMode from './SwitchMode'
+import messages from '@/utils/messages'
 
 function JobInfo ({ job }) {
   return (
@@ -46,10 +47,10 @@ function JobInfo ({ job }) {
   )
 }
 
-function JobEdit ({ job, handleSubmit, loading }) {
+function JobEdit ({ job, handleSubmit, loading, error }) {
   return (
     <form onSubmit={handleSubmit} className='grid grid-cols-2 gap-x-4 gap-y-2'>
-      <p>Categoria</p>
+      <p>Oficio</p>
       <p>{job.category}</p>
       <label htmlFor='hourlyPrice'>Precio por hora</label>
       <input type='number' min='0' max='200000000' step='1' name='hourlyPrice' id='hourlyPrice' defaultValue={job.hourly_price} className='text-black p-1 rounded-md' />
@@ -68,6 +69,7 @@ function JobEdit ({ job, handleSubmit, loading }) {
       <button disabled={loading} className='rounded-xl bg-brand6 px-4 py-2 font-semibold text-brand8 hover:bg-brand2 disabled:opacity-50' type='submit'>
         {loading ? 'Cargando...' : 'Guardar'}
       </button>
+      <span className={`${error ? 'block' : 'hidden'} text-red-600 bg-red-200 border-2 rounded-lg p-2 border-red-600`}>{error}</span>
     </form>
   )
 }
@@ -75,46 +77,54 @@ function JobEdit ({ job, handleSubmit, loading }) {
 function JobCard ({ job }) {
   const [editMode, setEditMode] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   const toggleEditMode = () => {
     setEditMode(!editMode)
+    setError(false)
   }
 
   const handleSubmit = async (jobdata) => {
     jobdata.preventDefault()
     setLoading(true)
+    setError(false)
 
-    const sendData = {
-      IdWorker: job.id_worker,
-      hourly_price: jobdata.target.hourlyPrice.value,
-      country: jobdata.target.country.value,
-      province: jobdata.target.province.value,
-      city: jobdata.target.city.value,
-      employees: jobdata.target.employees.value,
-      attention_hours: jobdata.target.attentionHours.value,
-      description: jobdata.target.description.value
+    if (jobdata.target.hourlyPrice.value === '' || jobdata.target.attentionHours.value === '' || jobdata.target.employees.value === '' || jobdata.target.city.value === '' || jobdata.target.province.value === '' || jobdata.target.country.value === '' || jobdata.target.description.value === '') {
+      setError(messages.error.form_field_required)
+      setLoading(false)
+    } else {
+      const sendData = {
+        IdWorker: job.id_worker,
+        hourly_price: jobdata.target.hourlyPrice.value,
+        country: jobdata.target.country.value,
+        province: jobdata.target.province.value,
+        city: jobdata.target.city.value,
+        employees: jobdata.target.employees.value,
+        attention_hours: jobdata.target.attentionHours.value,
+        description: jobdata.target.description.value
+      }
+
+      const response = await fetch('/api/update/worker', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(sendData)
+      })
+      const data = await response.json()
+
+      if (data.error) setError(data.error)
+      else window.location.reload()
+
+      setLoading(false)
     }
-
-    const response = await fetch('/api/update/worker', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(sendData)
-    })
-    const data = await response.json()
-
-    if (data.error) alert(data.error)
-    else window.location.reload()
-
-    setLoading(false)
   }
 
   return (
     <li className='p-4 border-2 mb-2 mx-2 rounded-xl border-brand4/40 flex flex-col gap-1'>
       <SwitchMode toggleEditMode={toggleEditMode} editMode={editMode} />
       {editMode
-        ? <JobEdit job={job} handleSubmit={handleSubmit} loading={loading} />
+        ? <JobEdit job={job} handleSubmit={handleSubmit} loading={loading} error={error} />
         : <JobInfo job={job} />}
     </li>
   )
--- src/components/ProposalCard.jsx
+++ src/components/ProposalCard.jsx
@@ -3,8 +3,9 @@
 import { useState } from 'react'
 import SwitchMode from './SwitchMode'
 import ProposalInfo from './ProposalInfo'
+import messages from '@/utils/messages'
 
-function ProposalEdit ({ proposal, handleSubmit, loading }) {
+function ProposalEdit ({ proposal, handleSubmit, loading, error }) {
   return (
     <form onSubmit={handleSubmit} className='grid grid-cols-2 gap-x-4 gap-y-2'>
       <p>Categoria</p>
@@ -22,6 +23,7 @@ function ProposalEdit ({ proposal, handleSubmit, loading }) {
       <button disabled={loading} className='rounded-xl bg-brand6 px-4 py-2 font-semibold text-brand8 hover:bg-brand2 disabled:opacity-50' type='submit'>
         {loading ? 'Cargando...' : 'Guardar'}
       </button>
+      <span className={`${error ? 'block' : 'hidden'} text-red-600 bg-red-200 border-2 rounded-lg p-2 border-red-600`}>{error}</span>
     </form>
   )
 }
@@ -29,47 +31,55 @@ function ProposalEdit ({ proposal, handleSubmit, loading }) {
 function ProposalCard ({ proposal }) {
   const [editMode, setEditMode] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   const toggleEditMode = () => {
     setEditMode(!editMode)
+    setError(false)
   }
 
   const handleSubmit = async (proposaldata) => {
     proposaldata.preventDefault()
 
     setLoading(true)
+    setError(false)
 
-    const sendData = {
-      IdProposal: proposal.id_proposal,
-      budget: proposaldata.target.budget.value,
-      country: proposaldata.target.country.value,
-      province: proposaldata.target.province.value,
-      city: proposaldata.target.city.value,
-      description: proposaldata.target.description.value
-    }
-    const response = await fetch('/api/update/proposal', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(sendData)
-    })
-    const data = await response.json()
-
-    if (data.error) {
-      alert(data.error)
+    if (proposaldata.target.budget.value === '' || proposaldata.target.city.value === '' || proposaldata.target.province.value === '' || proposaldata.target.country.value === '' || proposaldata.target.description.value === '') {
+      setError(messages.error.form_field_required)
+      setLoading(false)
     } else {
-      // If there is not any problem the page is refreshed
-      window.location.reload()
+      const sendData = {
+        IdProposal: proposal.id_proposal,
+        budget: proposaldata.target.budget.value,
+        country: proposaldata.target.country.value,
+        province: proposaldata.target.province.value,
+        city: proposaldata.target.city.value,
+        description: proposaldata.target.description.value
+      }
+      const response = await fetch('/api/update/proposal', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(sendData)
+      })
+      const data = await response.json()
+
+      if (data.error) {
+        setError(data.error)
+      } else {
+        // If there is not any problem the page is refreshed
+        window.location.reload()
+      }
+      setLoading(false)
     }
-    setLoading(false)
   }
 
   return (
     <li className='p-4 border-2 mb-2 mx-2 rounded-xl border-brand4/40 flex flex-col gap-1'>
       <SwitchMode toggleEditMode={toggleEditMode} editMode={editMode} />
       {editMode
-        ? <ProposalEdit proposal={proposal} handleSubmit={handleSubmit} loading={loading} />
+        ? <ProposalEdit proposal={proposal} handleSubmit={handleSubmit} loading={loading} error={error} />
         : <ProposalInfo proposal={proposal} />}
     </li>
   )
