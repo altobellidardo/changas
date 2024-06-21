0e6f2f14b33a030f35b9b3fe8f56e7f0abb11dfa
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-06-21 17:55:06 -0300

    erased empty div from message

--- src/app/chats/components/Message.jsx
+++ src/app/chats/components/Message.jsx
@@ -4,7 +4,6 @@ function Message ({ item, UserNumber }) {
       <div className={`rounded-lg p-4 mx-2 my-1 ${item.user_num !== parseInt(UserNumber) ? 'self-end border ' : 'self-start bg-brand3 text-brand8'}`}>
         {item.message}
       </div>
-      <div>{item.user_num === parseInt(UserNumber)}</div>
     </div>
   )
 }
