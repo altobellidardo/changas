2f0b25a80512d98a5c906a67b2f5a2264f6bf4d8
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-07-02 15:01:55 -0300

    refact legal

--- src/app/legal/page.jsx
+++ src/app/legal/page.jsx
@@ -2,24 +2,44 @@ import Footer from '@/components/footer'
 import Header from '@/components/header/header'
 import content from './content'
 
+function Element (element) {
+  switch (element.tag) {
+    case 'p':
+      return <p key={element.text} className='mb-2'>{element.text}</p>
+    case 'ul':
+      return (
+        <ul key={Math.random().toString()}>
+          {element.content.map(element => (
+            <li key={element.text.toString()} className='list-disc ml-10'>
+              {element.text}
+            </li>
+          ))}
+        </ul>
+      )
+    default:
+      return null
+  }
+}
+
 function Section (section) {
+  const contenido = section.content
   return (
-    <section className='my-10'>
-      <h2 className='text-2xl font-bold'>{section.title}</h2>
-      {section.content.map(paragraph => (
-        <p key={paragraph.slice(0, 10)}>{paragraph}</p>
-      ))}
+    <section>
+      <h2 className='text-xl font-bold mb-4'>{section.title}</h2>
+      {contenido.map(element => Element(element))}
     </section>
   )
 }
 
 function Legal () {
-  const { title, sections } = content.termsAndConditions
+  const sections = content.data
   return (
     <div className='flex flex-col min-h-screen justify-between'>
       <Header />
       <main className='mx-auto max-w-[80vw] my-10'>
-        <h1 className='text-3xl font-bold'>{title}</h1>
+        <h1 className='text-3xl font-bold'>
+          Términos y condiciones de uso de la plataforma Changas
+        </h1>
         {sections.map(section => Section(section))}
       </main>
       <Footer />
