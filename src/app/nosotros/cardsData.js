d8b3f52a2ca90c8585d11b57525ffe1ca1807323
Maximo Rulli <rullimaximoeduardo@gmail.com>
2024-07-03 13:52:37 -0300

    fixed nosotros

--- src/app/nosotros/cardsData.js
+++ src/app/nosotros/cardsData.js
@@ -1,21 +1,21 @@
 export const cards = [
   {
-    titulo: 'Conocé a Nuestro Equipo',
-    subtitulo: 'Somos un grupo de estudiantes en el proceso de creación de un proyecto que intenta ir mucho más allá que tan solo un sitio web.',
+    titulo: 'Nuestro Equipo',
+    subtitulo: 'Somos un grupo de jóvenes emprendedores en desarrollo de un emprendimiento que busca ayudar con el contexto actual de desempleo de Argentina. Buscamos que las personas puedan crecer como profesionales y que quienes los busquen puedan hallarlos con facilidad.',
     imagen: '/img/nosotros/nosotros_1.webp',
     width: 644,
     height: 461
   },
   {
     titulo: 'Nuestra Misión',
-    subtitulo: 'Transmitir que los sueños laborales y vitales son alacanzables con esfuerzo y herramientas adecuadas; y brindar el apoyo necesario para que otros también puedan lograr sus metas profesionales y personales.',
+    subtitulo: 'Transmitir que los sueños laborales y vitales son alcanzables con esfuerzo y herramientas adecuadas; y brindar el apoyo necesario para que otros también puedan lograr sus metas profesionales y personales.',
     imagen: '/img/nosotros/nosotros_2.webp',
     width: 653,
     height: 469
   },
   {
-    titulo: 'Nuestras Instalaciones',
-    subtitulo: 'Tenemos nuestra base en la ciudad Campana, en Buenos Aires, más precisamente en la Escuela Técnica Roberto Rocca.',
+    titulo: 'Nuestros valores',
+    subtitulo: 'Nos proponemos mediante diferentes métodos ofrecidos ser una plataforma confiable para nuestros usuarios. Nuestro equipo a su vez busca generar un bien en el usuario ayudándolo a desarrollarse como profesional y/o brindándole la oportunidad para contratar al profesional indicado para realizar un servicio.',
     imagen: '/img/nosotros/nosotros_3.webp',
     width: 644,
     height: 461
