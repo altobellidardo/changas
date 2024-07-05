eb924ca2e1a9d43ac50ede9a17abd65d26a3a380
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-07-05 16:45:50 -0300

    add chat test preview

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -2,10 +2,19 @@
 
 import supabase from '@/libs/supabase/server'
 
-export async function sendMessage (formData) {
-  const { message } = formData.get('msg')
-
-  const { data, error } = await supabase
+export async function getMessages () {
+  const { data: messages } = await supabase
     .from('chat-test')
     .select('*')
+    .order('id', { ascending: false })
+
+  return messages
+}
+
+export async function sendMessage (formData) {
+  const msg = formData.get('msg')
+
+  await supabase
+    .from('chat-test')
+    .insert({ msg, from: true })
 }
--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,40 +1,42 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
-import { sendMessage } from '@/actions/chat'
+import { getMessages, sendMessage } from '@/actions/chat'
 import createClient from '@/libs/supabase/client'
 import { useState, useEffect } from 'react'
 
 function ChatPage () {
-  const supabase = createClient()
   const [messages, setMessages] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  function handleNewMessage (payload) {
+    setMessages((messages) => [...messages, payload.new])
+  }
 
   useEffect(() => {
     const supabase = createClient()
 
-    async function subscribeChat () {
-      await supabase
-        .channel(
-          'chat',
-          {
-            config: {
-              
-            }
-          }
-        )
-    }
+    getMessages()
+      .then((data) => setMessages(data))
+      .finally(() => setLoading(false))
+
+    supabase
+      .channel('chat')
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'chat-test' }, handleNewMessage)
+      .subscribe()
   }, [])
 
   return (
     <main>
+      {loading && <p>Cargando...</p>}
       <ul>
         {messages.map((msg) => (
           <li key={msg.id}>{msg.msg}</li>
         ))}
-        <li>message</li>
       </ul>
 
       <form action={sendMessage}>
-        <input type='text' />
+        <input type='text' name='msg' />
         <button type='submit'>Enviar</button>
       </form>
     </main>
