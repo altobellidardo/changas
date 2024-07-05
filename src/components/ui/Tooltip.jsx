cbd207a55401dedc38b8bbc646a7ab0978bef92a
Maximo Rulli <rullimaximoeduardo@gmail.com>
2024-07-05 08:49:59 -0300

    shorter tooltip

--- src/components/ui/Tooltip.jsx
+++ src/components/ui/Tooltip.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Tooltip = ({ text }) => (
-  <div className='absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-40 bg-gray-700 text-white text-center rounded py-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100' style={{ transitionDelay: '500ms' }}>
+  <div className='absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-40 bg-gray-700 text-white text-center rounded py-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100' style={{ transitionDelay: '200ms' }}>
     {text}
   </div>
 )
