986682c35d5cd41f932aa2ae67088834c50cdb4a
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-08-02 14:14:47 -0300

    fix page navigation

--- src/app/postularse/[category]/page.jsx
+++ src/app/postularse/[category]/page.jsx
@@ -24,7 +24,7 @@ export default async function JobProposals ({ params }) {
     <main className='min-h-screen flex flex-col justify-between'>
       <Header />
       <section className='pt-10 w-[80vw] max-w-[1200px] mx-auto my-10'>
-        <Link href='/contratar' className='text-brand6 hover:underline mb-2'>Atrás</Link>
+        <Link href='/postularse' className='text-brand6 hover:underline mb-2'>Atrás</Link>
         <h1 className='text-3xl font-bold mb-4'>
           Encontrar trabajo de {category}
         </h1>
