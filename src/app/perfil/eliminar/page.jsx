9b447c323bd1248cf6090962da33e334a02b3e27
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-08-03 11:05:08 -0300

    updated delete account text

--- src/app/perfil/eliminar/page.jsx
+++ src/app/perfil/eliminar/page.jsx
@@ -25,7 +25,8 @@ function EliminarCuenta () {
       <main className='flex min-h-screen flex-col bg-brand8 justify-between'>
         <Header />
         <p className='font-bold text-xl mx-auto'>
-          Estas a punto de eliminar tu cuenta. Por favor confirma que deseas hacerlo.
+          Estas a punto de eliminar tu cuenta. Por favor confirma que deseas hacerlo. La eliminación de la cuenta puede demorar hasta 7 días hábiles, por lo que puede ser que sigas podiendo iniciar sesión e interactuar en la plataforma. Para conocer más información sobre como es que eliminamos tu cuenta y tus datos visita
+          <a className='text-blue-500 underline' href='/legal/privacidad'> nuestra Política de Privacidad</a>
         </p>
         <form action={sendEmailDeleteUser} className='flex w-full'>
           <button type='submit' className='text-red-600 bg-red-200 border-2 rounded-lg p-2 border-red-600 max-w-[600px] text-center mx-auto'>Eliminar cuenta</button>
