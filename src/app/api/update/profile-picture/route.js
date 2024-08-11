3e0a8c4662186dc5dfcf9ec12ab573b72923fc0d
Maximo Rulli <75349174+Maximo-Rulli@users.noreply.github.com>
2024-08-11 16:54:25 -0300

    Implemented profile picture update, lacks deletion

---
+++ src/app/api/update/profile-picture/route.js
@@ -0,0 +1,25 @@
+// Update profile picture endpoint
+import { decode } from 'base64-arraybuffer'
+import supabase from '@/libs/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function POST (request) {
+  const data = await request.json()
+
+  const userData = {
+    id_user: data.IdUser,
+    picture: data.image !== null // If the file size is 0 there is no such file
+  }
+  
+  // upload profile picture
+  if (userData.picture) {
+    const { error: profileFail } = await supabase.storage.from('profiles').upload(userData.id_user, decode(data.image.base64), {
+      upsert: true, contentType: 'image/jpeg' // The Expo app sends all images as JPEG
+    })
+    if (profileFail) {
+      return NextResponse.json({ error: 'Error al cargar foto' })
+    }
+  }
+
+  return NextResponse.json({ status: 200 })
+}
