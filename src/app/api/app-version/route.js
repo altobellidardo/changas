e56ed671be3c64809f843e537d1777d1debf5e2d
Maximo Rulli <rullimaximoeduardo@gmail.com>
2024-08-15 09:41:41 -0300

    updated route and version

--- src/app/api/app-version/route.js
+++ src/app/api/app-version/route.js
@@ -1,5 +1,5 @@
 export function GET () {
-  return new Response(JSON.stringify({ version: '1.0.0' }), {
+  return new Response(JSON.stringify({ version: '1.0.4' }), {
     headers: {
       'Content-Type': 'application/json'
     }
