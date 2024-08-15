dfa28a87be357c4a9cca05d6a0b412ff6008bbe0
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-08-15 14:46:38 -0300

    remove service key

--- src/libs/supabase/server.js
+++ src/libs/supabase/server.js
@@ -2,8 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  // process.env.SUPABASE_SERVICE_KEY
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjb2pyYWpzbWpobXludnR1cWt5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTE0MDcwNiwiZXhwIjoyMDI2NzE2NzA2fQ.asmu2yye0hRQ9EGIlLb7boJhTrfoheoPKYZ8lqMutk4'
+  process.env.SUPABASE_SERVICE_KEY
 )
 
 export default supabase
