0e76aaaa65b02a69612d9e7a2e20af0a3312a933
Dardo Altobelli <altobellidardo@gmail.com>
2024-08-31 13:15:24 -0300

    hide delete account

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -89,7 +89,10 @@ export default async function UserPage () {
         </section>
 
         <section className='mt-32 flex flex-col gap-2'>
-          <Link href='/perfil/eliminar' className='text-red-600 bg-red-200 border-2 rounded-lg p-2 border-red-600 max-w-[600px] text-center font-bold'>
+          <Link
+            href='/perfil/eliminar'
+            className='text-red-600 bg-red-100 rounded-lg w-fit p-1 opacity-70'
+          >
             Eliminar cuenta
           </Link>
         </section>
