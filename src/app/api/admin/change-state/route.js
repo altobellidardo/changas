f752c3d9ee92a4bc55a653edd61df3533fd57ccc
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-09-05 16:46:54 -0300

    add verification

--- src/app/api/admin/change-state/route.js
+++ src/app/api/admin/change-state/route.js
@@ -4,6 +4,21 @@ import checkUser from '@/utils/checkUser'
 import messages from '@/utils/messages'
 import { NextRequest, NextResponse } from 'next/server'
 
+export async function GET () {
+  const id = '3789d804-f6ba-429d-bcbc-b7a5374132cb'
+  const status = 'rejected'
+
+  const { data, error } = await supabase
+    .from('users_data')
+    .update({ status })
+    .eq('id_user', id)
+
+  if (error) console.log(error)
+  if (data) console.log(data)
+
+  return NextResponse.json({ id })
+}
+
 export async function POST (request) {
   const req = new NextRequest(request)
 
@@ -28,7 +43,7 @@ export async function POST (request) {
       return NextResponse.json({ error: messages.error.failed_user_update }, { status: 404 })
     }
 
-    const { error } = supabase
+    const { error } = await supabase
       .from('users_data')
       .update({ status })
       .eq('id_user', idUser)
@@ -36,6 +51,8 @@ export async function POST (request) {
     if (error) {
       return NextResponse.json({ error: messages.error.failed_user_update }, { status: 404 })
     }
+
+    return NextResponse.json({ message: 'Success' }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error: 'Invalid request, no body' }, { status: 406 })
   }
