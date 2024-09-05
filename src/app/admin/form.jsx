1144035a1570b33a41aba911422e8f32f95181b6
Dardo Altobelli <daltobelli@etrr.edu.ar>
2024-09-05 14:52:26 -0300

    update route

--- src/app/admin/form.jsx
+++ src/app/admin/form.jsx
@@ -19,12 +19,12 @@ export function Actions ({ userId }) {
   return (
     <section className='flex gap-8 mx-auto my-10'>
       <button
-        onClick={() => action('approve')}
+        onClick={() => action('verified')}
         className='border border-brand3 bg-brand3/30 text-brand3 px-4 py-2 font-bold rounded-md hover:bg-brand3/50'
       >Aceptar
       </button>
       <button
-        onClick={() => action('reject')}
+        onClick={() => action('rejected')}
         className='border border-red-500 bg-red-200/30 text-red-500 px-4 py-2 font-bold rounded-md hover:bg-red-500/50'
       >Rechazar
       </button>
--- src/app/api/admin/change-state/route.js
+++ src/app/api/admin/change-state/route.js
@@ -1,16 +1,42 @@
+import { ADMINS } from '@/constants'
 import supabase from '@/libs/supabase/server'
+import checkUser from '@/utils/checkUser'
 import messages from '@/utils/messages'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST (req) {
-  const { idUser, status } = await req.json()
+export async function POST (request) {
+  const req = new NextRequest(request)
 
-  const { error } = supabase
-    .from('users_data')
-    .update({ status })
-    .eq('id_user', idUser)
+  try {
+    const body = await req.json()
+    const { idUser, status } = body
+    if (!idUser || !status) {
+      return NextResponse.json({ error: 'Invalid request' }, { status: 404 })
+    }
 
-  if (error) {
-    return NextResponse.json({ error: messages.error.failed_user_update }, { status: 404 })
+    const token = req.cookies.get('token')
+    if (!token || token.value === '') {
+      return NextResponse.json({ message: 'Not valid token' })
+    }
+    const isValidToken = checkUser(token.value)
+    if (!isValidToken) {
+      return NextResponse.json({ message: 'Invalid token' })
+    }
+
+    const userEmail = isValidToken.email
+    if (!ADMINS.includes(userEmail)) {
+      return NextResponse.json({ error: messages.error.failed_user_update }, { status: 404 })
+    }
+
+    const { error } = supabase
+      .from('users_data')
+      .update({ status })
+      .eq('id_user', idUser)
+
+    if (error) {
+      return NextResponse.json({ error: messages.error.failed_user_update }, { status: 404 })
+    }
+  } catch (error) {
+    return NextResponse.json({ error: 'Invalid request, no body' }, { status: 406 })
   }
 }
